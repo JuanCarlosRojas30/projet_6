4b6a89042ab2dc9b152dc8a5afea39f6608094a4
JuanCarlosRojas30 <rrunsiman@gmail.com>
2023-10-07 08:22:38 +0200

    modification filters

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,28 +1,68 @@
 let worksData = [];
+let categoryData = [];
+
 
 const gallery = document.querySelector(".gallery");
+let filters = document.querySelector(".filters");
+
+let currentCat = 0;
 
 
-fetch("http://localhost:5678/api/works") // demande des donnes de backend
-.then((res) => res.json()) // on convert en json
-.then((data) => {
+// get works
+const getWorks = () => {
 
-    worksData = data;
-    console.log(worksData);
-    displayWorks;
+    fetch("http://localhost:5678/api/works")
+    .then((res) => res.json()) 
+    .then((data) => {
+        worksData = data;
+        console.log(worksData);
+        displayWorks();
 
-}) 
 
+})
 .catch((err) => console.log(err, "fetch error"));
+}
 
-const displayWorks = () => {
-    gallery.innerHTML = `
-    <figure>
-    <img src="assets/images/abajour-tahina.png" alt="Abajour Tahina">
-    <figcaption>Abajour Tahina</figcaption>
-    </figure>`
 
+// get category
+const getCategory = () => {
+    fetch("http://localhost:5678/api/categories")
+    .then((res) => res.json()) 
+    .then((data) => {
+        categoryData = data;
+        //console.log(worksData);
+        displayFiltres(); 
+
+})
+.catch((err) => console.log(err, "fetch error"));
+}
+
+
+
+//affichage works
+const displayWorks = () => 
+{
+
+    gallery.innerHTML += "";
+    
+    worksData.forEach(work => {
+
+    if(currentCat===work.categoryId )
+        gallery.innerHTML += `
+        <figure>
+        <img src="${work.imageUrl}" alt="${work.title}">
+        <figcaption>${work.title}</figcaption>
+        </figure>`
+    } )
 } 
 
+//affichage categories
+const displayFiltres = () => {
+    categoryData.forEach(category => {
+        filters.innerHTML += `<button class="filter__btn">${category.name}</button>`
 
+    } )
+} 
 
+getWorks();
+getCategory();
