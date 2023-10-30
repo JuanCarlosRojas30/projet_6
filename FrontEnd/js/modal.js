1fcd05b3d2f4fa495b8711b7cef61fbe045d109b
JuanCarlosRojas30 <rrunsiman@gmail.com>
2023-10-30 20:13:13 +0100

    recopie du code

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -10,7 +10,7 @@ const displayWorksModal = () =>
     modalWorks.innerHTML += `
         <div>
             <img src="${work.imageUrl}" alt="${work.title}">
-            <span><i class="fa fa-trash"></i></span> 
+            <span><i class="fa fa-trash" data-id="${work.id}"></i></span> 
         </div>`
         if (i === worksData.length-1) {
 
@@ -18,9 +18,27 @@ const displayWorksModal = () =>
     
             for (let j = 0; j < suppBtns.length; j++){
                 suppBtns[j].addEventListener("click", () =>  {
-                    alert(j)
+                    //alert(suppBtns[j].getAttribute("data-id"))
+                    const id = suppBtns[j].getAttribute("data-id");
+                    if (confirm("Press a button!") === true) {
+                        supprimeWork(id)
+                    }
                 });
             }
         } 
     } )
 } 
+
+const supprimeWork = (id) => {
+    fetch("http://localhost:5678/api/works" + id,{
+        method: "DELETE",
+        headers: {
+            Authorization: "Bearer" + JSON.parse(localStorage.getItem("info")).token, 
+        },
+    })
+    .then((response) => {
+        console.log(response);
+        getWorks();
+    })
+    .catch((err) => console.log(err, "fetch error"));
+};
